c620fa5c45e5c71af35a4510366537ebad07cba5
WalterDev76 <D310051@edu.curio.nl>
2025-10-08 14:27:53 +0200

    tabel maken in de overview page

--- tasks/overview.php
+++ tasks/overview.php
@@ -21,7 +21,35 @@
         </header>
     </section>
 
+     <div class="table_container">
+        <?php
+        require_once '../backend/conn.php';
+        $query = "SELECT * FROM taken WHERE status != 'done' ";
+        $statement = $conn->prepare($query);
+        $statement->execute();
+        $taken = $statement->fetchAll(PDO::FETCH_ASSOC);
 
+        
+        ?>
+
+        <table>
+            <tr>
+                <th>Titel</th>
+                <th>Afdeling</th>
+                <th>Status</th>
+
+
+            </tr>
+            <?php foreach($taken as $taak):?>
+                <tr>
+                    <td><?php echo $taak['titel']; ?></td>
+                    <td><?php echo $taak['afdeling']; ?></td>
+                    <td><?php echo $taak['status']; ?></td>
+                </tr>
+            
+            <?php endforeach; ?>
+        </table>
+     </div>
 
 </body>
 </html>
