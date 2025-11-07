7da5d8e7f19ad0927825b2e22d60aa89e3419e7e
AlaaAldeen05 <d302707@edu.curio.nl>
2025-11-07 11:47:39 +0100

    Update afdeling.php
    afdeling.php aangepast

--- tasks/afdeling.php
+++ tasks/afdeling.php
@@ -10,18 +10,7 @@
 </head>
 
 <body class="tasks_index_body">
-    <section class="header">
-        <header>
-            <div class="wrapper">
-                <img class="header_logo" src="../img/logo-big-v2.png" alt="">
-                <nav>
-                    <a href="../index.php">Home</a>
-                    <a href="index.php">Takenlijst</a>
-                </nav>
-            </div>
-        </header>
-    </section>
-
+    
     <div class="afdeling_page">
         <div class="afdeling_header">
             <h1>Overzicht per afdeling</h1>
